102977b2edfbbadc4d9643b1adc8b3d4d2106ca8
NataliaGatarek <nova.nathalia@gmail.com>
2021-07-18 15:03:55 +0200

    First try to create LogIn View

---
+++ client/src/Views/logIn.tsx
@@ -0,0 +1,93 @@
+import React, {
+  FC,
+  Fragment,
+  useState,
+  ChangeEvent,
+  FormEvent,
+  useContext
+} from "react";
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      width: 400,
+      margin: `${theme.spacing(0)} auto`
+    },
+    loginBtn: {
+      marginTop: theme.spacing(2),
+      flexGrow: 1
+    },
+    header: {
+      textAlign: 'center',
+      background: '#212121',
+      color: '#fff'
+    },
+    card: {
+      marginTop: theme.spacing(10)
+    }
+  })
+);
+
+
+interface Props {
+  username: string,
+  password: string,
+}
+
+const logIn: React.FC<Props> = (props) => {
+const classes = useStyles();
+/* const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    username(e.target.value); <== how to setUserName??????
+  }; */
+  return (
+    <>
+     <form className={classes.container} noValidate autoComplete="off">
+      <Card className={classes.card}>
+        <CardHeader className={classes.header} title="Login App" />
+        <CardContent>
+          <div>
+            <TextField
+              fullWidth
+              id="username"
+              type="email"
+              label="Username"
+              placeholder="Username"
+              margin="normal"
+              //onChange={handleUsernameChange}
+            />
+            <TextField
+              fullWidth
+              id="password"
+              type="password"
+              label="Password"
+              placeholder="Password"
+              margin="normal"
+            />
+          </div>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="contained"
+            size="large"
+            color="secondary"
+            >
+            Login
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
+    </>
+  );
+};
+
+export default logIn;
