1e7d9e4fa6fa83acadccff0ab34e0846cc52dd55
OBenshi <65823304+OBenshi@users.noreply.github.com>
2021-07-21 19:07:25 +0200

    hi

--- client/src/GraphQL/Mutations.tsx
+++ client/src/GraphQL/Mutations.tsx
@@ -1,4 +1,5 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery } from "@apollo/client";
+
 /* 
 export const LOGIN_USER = gql`
   mutation addUser ($email: String!,$password: String!) {
@@ -7,5 +8,18 @@ export const LOGIN_USER = gql`
     }
   }
 `; */
+//*Good morning dearest Nathalia,
+// if you are writing comments meant for me to read it would be better if you
+// would use anchor comments with any workspace scope
+// tag(TODO,REVIEW,FIXME,LINK,SECTION/!SECTION).
+// Like this:
 
-//mutation for login, 1. wrote for now "user" but it should be name of the route from the back end. at the end I am returning only email//
+// REVIEW: mutation for login, 1. wrote for now "user" but it should be name of the
+// route from the back end. at the end I am returning only email//
+
+// if we combine it with the better comments extension behavior we could even
+// make it colorful(and more A.D.D friendly...) by adding */!/? like this:
+
+//* Nathalia is awesome
+//! WARNING Nathalia is too awesome
+//? can anyone be as awesome as Nathalia?
