fc4aefdf87f3b5efcd8fddc9c3e09ed6a5e52389
NataliaGatarek <nova.nathalia@gmail.com>
2021-07-26 13:56:53 +0200

    Displaying dating text

--- client/src/GraphQL/Queries.tsx
+++ client/src/GraphQL/Queries.tsx
@@ -1,2 +1,16 @@
 import { gql, useQuery } from '@apollo/client';
 
+export const DATING_TEXT = gql`
+query Query {
+  allTexts {
+    _id
+    owner
+    text
+    score
+    postDate
+    comments
+    display
+    private
+  }
+}
+`
