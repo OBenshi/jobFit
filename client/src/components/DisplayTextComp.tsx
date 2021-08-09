36bf1734275418b9b4c9fe5f39003eea562bac76
June-yu09 <yuzatee@gmail.com>
2021-08-09 23:56:51 +0200

    .

--- client/src/components/DisplayTextComp.tsx
+++ client/src/components/DisplayTextComp.tsx
@@ -12,28 +12,53 @@ import { useMutation } from "@apollo/client";
 import {ADD_COMMENT} from '../GraphQL/Mutations'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { Paper } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import TextField from "@material-ui/core/TextField";
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 
-const useStyles = makeStyles({
-  root: {
-        maxWidth: 340,
-        minHeight: 110,
-        marginBottom: 10,
-        marginLeft: 10,
-        elevation: 12,
+
+import { borders } from '@material-ui/system';
+import Box from '@material-ui/core/Box';
+import StarRateIcon from '@material-ui/icons/StarRate';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
+const useStyles = makeStyles((theme) => ({
+  small: {
+    maxWidth: 320,
+    minHeight: 50,
+    backgroundColor: "#fffde7",
+    border: "1px",
+    borderRadius: "15%",
+    marginLeft:"30px"
   },
-  pos: {
-    marginBottom: 12,
+  grey: {
+    color: "red",
   },
-});
+  marginIcon: {
+    marginLeft: '30px',
+  },
+  big: {
+    margin: theme.spacing(1),
+    maxWidth: 340,
+    minHeight: 120,
+    border: "2px",
+    borderRadius: "7%",
+    borderColor: "red",
+    backgroundColor: "#e0e0e0",
+  },
+  dating: {
+    backgroundColor: "#fffde7",
+    padding:theme.spacing(2)
+  },
+  biggest: {
+    backgroundColor: "#e0e0e0"
+  },
+   textField: {
+     width: '320px',
+     backgroundColor: "#e0e0e0",
+  }
+   
+}));
 
 interface IAddComment {
    //owner: string,
@@ -56,55 +81,87 @@ const DisplayTextComp: React.FC<Props> = (props) => {
   const handleChange =  (e: ChangeEvent<any>) =>
   setComment({ ...comment, [e.target.name]: e.target.value }
     ); 
-  const handleSubmit =  (e: FormEvent<HTMLFormElement>)=> {
-    e.preventDefault();
-    addComment({
-      variables: {
-        "addCommentComment": {
-          //"owner": comment.owner,
-          "text": comment.text,
-          //"score": comment.score,
-          //"onText": comment.onText
-      }
-        }
-      }
-    )
-    if (error) {
-      console.log(error)
-    } else {
-      console.log("success")
-    }
-    }  
+ const handleSubmit =  (e: FormEvent<HTMLFormElement>)=> {
+   e.preventDefault();
+        addComment({
+          variables: {
+            "addCommentComment": {
+             //"owner": comment.owner,
+             "text": comment.text,
+             //"score": comment.score,
+             //"onText": comment.onText
+          }
+            }
+          }
+        )
+        if (error) {
+          console.log(error)
+        } else {
+          console.log("success")
+   }
+      }  
   console.log(data);
-  //console.log(props.allText.owner.username);
   console.log(props.allText.postDate);
   return (
-      <Card className={classes.root}>
-        <CardContent>
-          <Typography gutterBottom variant="caption" display="block" style={{ textAlign: "left" }} >Owner {/* {props.allText.owner.username} */}</Typography>
-          <Typography gutterBottom variant="h6">{props.allText.text}</Typography>
-          <Typography gutterBottom variant="caption" display="block" style={{textAlign:"left"}}>{props.allText.postDate}</Typography>
-         {data !== undefined && props.allText.comments.map((comment: any) => {
-           return <div style={{ backgroundColor: "#FFFF00", border: "2px solid red" }}>
-             <Typography variant="caption" display="block" gutterBottom style={{textAlign:"left"}}>owner</Typography>
-             <Typography variant="body2">{comment.text}</Typography><div>
-            <Typography variant="caption" display="block" gutterBottom style={{textAlign:"right"}}>date </Typography>
-           </div></div>
-         })}
-          <hr></hr>
-        </CardContent>
-        <CardActions>
-          <form noValidate autoComplete="off" onSubmit={handleSubmit} style={{display: "flex", alignItems:"center"}}>
-          <TextareaAutosize
+ <div><Box className={classes.big}>
+        <Box display="flex"ml={1} >
+        <FavoriteIcon style={{padding:"4px"}}/>
+            <Box display="flex">
+              <Box ml={2}>
+              <Typography variant="h5" gutterBottom >
+                username
+              </Typography>
+            </Box>
+          </Box>
+        <Box ml={5}>
+          <Typography variant="caption" display="block" gutterBottom className={classes.grey}>{props.allText.postDate}</Typography>
+        </Box>
+      </Box>
+      <Typography variant="body1" gutterBottom className={classes.dating}>{props.allText.text}</Typography>
+    </Box>
+    {/* 2nd  */}
+    {data !== undefined && props.allText.comments.map((comment: any) => {
+      return <Box className={classes.small}>
+          <Box display="flex" >
+          <StarRateIcon/>
+            <Box display="flex">
+              <Box ml={4}>
+              <Typography variant="h5" gutterBottom>
+                owner {comment.owner}
+              </Typography>
+            </Box>
+          </Box>
+        <Box ml={5}>
+          <Typography variant="caption" display="block" className={classes.grey}>{comment.postDate}</Typography>
+        </Box>
+      </Box>
+      <Typography variant="body2">{comment.text}</Typography>
+    </Box>
+    })}
+      <Box>
+          <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+ {/*          <TextareaAutosize
             aria-label="minimum height"
-              minRows={6} placeholder="your comment"
+              minRows={1} placeholder="your comment"
               name="text"
               value={comment.text}
-              onChange={handleChange} /><hr></hr>
-            <Button size="small" variant="contained" style={{ backgroundColor: "#FFD700", color: '#FFFFFF' }} type="submit">Add a comment</Button>
+              onChange={handleChange} /> */}
+          <TextField
+            className={classes.textField}
+            label="comment"
+            id="standard-size-small"
+            defaultValue="comment"
+            size="small"
+            rows={1}
+            name="text"
+            value={comment.text}
+            variant="outlined"
+            onChange={handleChange} 
+      />
+            <Button size="small" variant="contained" style={{ backgroundColor: "#FFD700", color: '#FFFFFF'}} type="submit">post</Button>
             </form>
-      </CardActions>
-      </Card>
+     </Box>
+  </div >
   )
 }
 export default DisplayTextComp;
