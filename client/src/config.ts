6c102618c76e8c39709a53d0539eca41111af352
OBenshi <65823304+OBenshi@users.noreply.github.com>
2021-09-20 19:40:12 +0200

    typo fix

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
 export const serverURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000'
-    : 'https://swat-analyzer.herokuapp.com/';
+    : 'https://swat-analyzer.herokuapp.com';
