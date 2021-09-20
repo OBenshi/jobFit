13a2386ae725b2634bb511678f22b08f9e2c148b
OBenshi <65823304+OBenshi@users.noreply.github.com>
2021-09-20 19:36:43 +0200

    idk

--- server/src/server.ts
+++ server/src/server.ts
@@ -26,7 +26,7 @@ async function startApolloServer() {
     const app = express();
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
-    app.use(cors(corsOptions));
+    app.use(cors());
     // app.use("/users", require("./routes/users"));
     server.applyMiddleware({
       app,
